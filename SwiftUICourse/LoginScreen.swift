6228312b1749c10192805ed39ef6679b6bc85b19
IlyaRekhtin <76813341+IlyaRekhtin@users.noreply.github.com>
2022-11-06 00:03:38 +0300

    init

---
+++ SwiftUICourse/LoginScreen.swift
@@ -0,0 +1,60 @@
+
+//  SwiftUICourse
+//
+//  Created by Илья Рехтин on 05.11.2022.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    @State private var login = ""
+    @State private var password = ""
+    
+    var body: some View {
+            ZStack {
+                Image("startImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay {
+                        Color.black
+                            .opacity(0.4)
+                    }
+                    .blur(radius: 3.0)
+                VStack{
+                    TextField("Enter your login", text: $login)
+                        .textFieldStyle(.roundedBorder)
+                        .padding([.leading, .trailing])
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                        .padding([.leading, .trailing])
+                        .padding(.bottom, 20)
+                    Button {
+                        print("login")
+                    } label: {
+                        Text("LogIn")
+                            .font(.title)
+                        Image(systemName: "chevron.right")
+                            .font(.title)
+                    }.buttonStyle(.plain)
+                        .textFieldStyle(.roundedBorder)
+                        .foregroundColor(.white)
+                }
+                
+            }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
+            .previewDevice("iPhone 12")
+            .previewInterfaceOrientation(.portrait)
+    }
+}
---
+++ SwiftUICourse/SwiftUICourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICourseApp.swift
+//  SwiftUICourse
+//
+//  Created by Илья Рехтин on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginScreen()
+        }
+    }
+}
