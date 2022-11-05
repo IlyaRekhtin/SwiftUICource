ee298075acc012fb03c6f408c69136a11e8d3eaa
IlyaRekhtin <76813341+IlyaRekhtin@users.noreply.github.com>
2022-11-06 01:21:43 +0300

    compl

---
+++ SwiftUICourse/FriendViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FriendViewModel.swift
+//  SwiftUICourse
+//
+//  Created by Илья Рехтин on 06.11.2022.
+//
+
+import SwiftUI
+
+struct FriendViewModel {
+    let id: Int
+    let avatar: Image
+    let name: String
+    let city: String
+}
